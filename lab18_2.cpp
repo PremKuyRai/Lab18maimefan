cfd54901a3114502297cc7e0b63b152f8bb55bcd
PremKuyRai <koravit_dilok@cmu.ac.th>
2026-02-17 13:44:21 +0700

    finish

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,9 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *a, Rect *b){
+    double x = min(a->x+a->w, b->x+b->w) - max(a->x, b->x);
+    double y = min(a->y, b->y) - max(a->y-a->h, b->y-b->h);
+    if (x>0 && y>0) return x*y;
+    return 0;
 }
